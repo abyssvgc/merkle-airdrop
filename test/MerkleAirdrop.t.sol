62e21d8c80fe01b5055c947508e2f56eed61dfaa
abyssvgc <abdeliniaramayo@gmail.com>
2025-07-18 11:22:12 -0400

    feat: Enhance MerkleAirdrop with EIP712 signature verification and update tests

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -19,6 +19,7 @@ contract MerkleAirdropTest is ZkSyncChainChecker, Test {
         bytes32(0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a),
         bytes32(0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576)
     ];
+    address public gasPayer;
     address user;
     uint256 userPrivKey;
 
@@ -33,13 +34,17 @@ contract MerkleAirdropTest is ZkSyncChainChecker, Test {
             token.transfer(address(airdrop), AMOUNT_TO_SEND);
         }
         (user, userPrivKey) = makeAddrAndKey("user");
+        gasPayer = makeAddr("gasPayer");
     }
 
     function testUserCanClaim() public {
         uint256 startingBalance = token.balanceOf(user);
+        bytes32 digest = airdrop.getMessageHash(user, AMOUNT_TO_CLAIM);
 
-        vm.prank(user);
-        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivKey, digest);
+
+        vm.prank(gasPayer);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF, v, r, s);
 
         uint256 endingBalance = token.balanceOf(user);
         console.log("User balance after claim:", endingBalance);
